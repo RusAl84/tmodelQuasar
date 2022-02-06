81b6878b20ec71dfc79f0b19eb3fb01e3c048d9f
Aleksey <rusal@bk.ru>
2022-02-06 19:07:53 +0300

    init

---
+++ src/api/api.js
@@ -0,0 +1,15 @@
+﻿import Axios from 'axios'
+
+const axios = Axios.create({
+    baseURL: 'http://localhost:5000',
+})
+
+export default class API {
+    static async sendMessage(text, topic_num) {
+        const resp = await axios.post(`/tmodel`, {
+            text: text,
+            topic_num: topic_num,
+        })
+        return resp.data
+    }
+}
